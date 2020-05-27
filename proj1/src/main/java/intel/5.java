5ba8a7f5dd564ecb4a4501154dac6df20b6dc41d
root <root@ip-172-31-45-43.us-east-2.compute.internal>
2020-05-27 05:56:56 +0000

    move pom.xml

---
+++ proj1/src/main/java/intel/5.java
@@ -0,0 +1,18 @@
+
+package intel;
+
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+
+
+/**
+ * Displays the last log entries
+ */
+@Command(scope = "cghcnbvhjgkjjlij", name = "5", description = "mbmvnvn")
+public class 5 extends OsgiCommandSupport {
+
+    protected Object doExecute() throws Exception {
+         System.out.println("Executing command 5");
+         return null;
+    }
+}
---
+++ proj1/src/test/java/intel/5Test.java
@@ -0,0 +1,28 @@
+
+package intel;
+
+
+import junit.framework.TestCase;
+
+/**
+ * Test cases for {@link 5}
+ */
+@SuppressWarnings("unchecked")
+public class 5Test extends TestCase {
+    
+    private 5 command;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+	public void testCommand() {
+
+	}
+}
